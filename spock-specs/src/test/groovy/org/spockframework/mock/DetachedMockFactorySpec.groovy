c97a939f003883627120b2c8e267425fbbb110ca
Leonard Brünings <lord_damokles@gmx.net>
2015-11-14 11:44:45 +0100

    Finish DetachedMockFactoySpec

--- spock-specs/src/test/groovy/org/spockframework/mock/DetachedMockFactorySpec.groovy
+++ spock-specs/src/test/groovy/org/spockframework/mock/DetachedMockFactorySpec.groovy
@@ -40,6 +40,68 @@ class DetachedMockFactorySpec extends Specification {
     detach(mock)
   }
 
+  def "Stub(class)"() {
+    given:
+    IMockMe stub = factory.Stub(IMockMe)
+    attach(stub)
+    stub.foo(2) >> 4
+
+    expect:
+    stub.foo(2) == 4
+    stub.foo(1) == 0
+    getMockName(stub) == 'IMockMe'
+
+    cleanup:
+    detach(stub)
+  }
+
+  def "Stub(options, class)"() {
+    given:
+    IMockMe stub = factory.Stub(IMockMe, name: 'customName')
+    attach(stub)
+    stub.foo(2) >> 4
+
+    expect:
+    stub.foo(2) == 4
+    stub.foo(1) == 0
+    getMockName(stub) == 'customName'
+
+    cleanup:
+    detach(stub)
+  }
+
+  def "Spy(class)"() {
+    given:
+    IMockMe spy = factory.Spy(MockMe.class)
+    attach(spy)
+
+    when:
+    int result = spy.foo(2)
+
+    then:
+    result == 1
+    1 * spy.foo(2)
+
+    cleanup:
+    detach(spy)
+  }
+
+  def "Spy(options, class)"() {
+    given:
+    IMockMe spy = factory.Spy(MockMe.class, constructorArgs: [42])
+    attach(spy)
+
+    when:
+    int result = spy.foo(2)
+
+    then:
+    result == 42
+    1 * spy.foo(2)
+
+    cleanup:
+    detach(spy)
+  }
+
   private String getMockName(IMockMe mock) {
     new MockUtil().asMock(mock).name
   }
@@ -54,5 +116,19 @@ class DetachedMockFactorySpec extends Specification {
 }
 
 interface IMockMe {
-  def foo(int i)
+  int foo(int i)
+}
+
+class MockMe implements IMockMe {
+  private int defaultAnswer = 1
+
+  MockMe(){}
+
+  MockMe(int defaultAnswer) {
+    this.defaultAnswer = defaultAnswer
+  }
+
+  int foo(int i) {
+    defaultAnswer
+  }
 }
