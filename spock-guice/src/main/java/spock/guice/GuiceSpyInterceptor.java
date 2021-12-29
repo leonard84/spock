c3e7b6f8d64286f3c8d35ca9e8b5fe5781a2a1d4
Leonard Brünings <leonard.bruenings@gradle.com>
2021-12-29 18:23:19 +0100

    Add documentation

--- spock-guice/src/main/java/spock/guice/GuiceSpyInterceptor.java
+++ spock-guice/src/main/java/spock/guice/GuiceSpyInterceptor.java
@@ -2,6 +2,16 @@ package spock.guice;
 
 import java.lang.annotation.*;
 
+/**
+ * Instructs Guice to create a spy for the annotated field.
+ * <p>
+ * Guice spies will behave like global mocks, in that they will intercept all calls to instances of the same type.
+ * In fact, they will also intercept calls to subtypes of the given type, as long as the methods are present in the
+ * type of the annotated field.
+ *
+ * @since 2.2
+ * @author Leonard Brünings
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface GuiceSpyInterceptor {
